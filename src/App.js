9c7c3193a1186b0067acdb578f620c0f00fc416d
GuilhermeAugustoR <guilhermereiscampos23@gmail.com>
2021-09-08 09:04:45 -0300

    Primeiro Commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+import User from './Components/User/User';
+import { UserStorage } from './UserContext';
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login/*" element={<Login />} />
+            <ProtectedRoute path="conta/*" element={<User />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
+import { ReactComponent as Dogs } from '../Assets/dogs.svg';
+import { UserContext } from '../UserContext';
+
+const Header = () => {
+  const { data } = React.useContext(UserContext);
+  return (
+    <div className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" arial-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        {data ? (
+          <Link className={styles.login} to="/conta">
+            {data.nome}
+          </Link>
+        ) : (
+          <Link className={styles.login} to="/login">
+            Login / Criar
+          </Link>
+        )}
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
